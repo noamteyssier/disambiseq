cbc9a58c11c10f184348d085cf546de2d178c75e
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-10-10 16:38:50 -0700

    added docs and instructions on readme

--- README.md
+++ README.md
@@ -2,3 +2,76 @@
 
 Creates unambiguous nucleotide mismatch libraries for
 for a set of nucleotide sequences.
+
+## Usage
+
+I've rewritten this functionality a few times for different use cases
+and put it into a standalone crate since it might be useful to others.
+
+This is used to generate unambiguous one-off mismatch libraries for
+a set of DNA sequences.
+
+### Creating a new unambiguous set
+
+```rust
+use disambiseq::Disambiseq;
+
+let sequences = vec![
+    "ACT".to_string(),
+    "AGT".to_string()
+];
+let dsq = Disambiseq::from_slice(&sequences);
+println!("{:#?}", dsq);
+```
+
+### Visualizing the set
+
+```text
+Disambiseq {
+    unambiguous: {
+        "TCT": "ACT",
+        "ACA": "ACT",
+        "CCT": "ACT",
+        "ACC": "ACT",
+        "CGT": "AGT",
+        "GGT": "AGT",
+        "AGA": "AGT",
+        "GCT": "ACT",
+        "ACG": "ACT",
+        "TGT": "AGT",
+        "AGC": "AGT",
+        "AGT": "ACT",
+        "AGG": "AGT",
+    },
+    parents: {
+        "AGT",
+        "ACT",
+    },
+    ambiguous: {
+        "ATT",
+        "AAT",
+    },
+}
+```
+
+### Querying the Set
+
+```rust
+use disambiseq::Disambiseq;
+
+let sequences = vec![
+    "ACT".to_string(),
+    "AGT".to_string()
+];
+let dsq = Disambiseq::from_slice(&sequences);
+
+// retrieve a parental sequence
+assert_eq!(dsq.get_parent("ACT"), Some(&"ACT".to_string()));
+
+// retrieve a mutation sequence's parent
+assert_eq!(dsq.get_parent("TCT"), Some(&"ACT".to_string()));
+
+// exclude sequences with ambiguous parents
+assert_eq!(dsq.get_parent("AAT"), None);
+assert_eq!(dsq.get_parent("ATT"), None);
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,80 @@
+//! # disambiseq
+//!
+//! ## Background
+//!
+//! I've rewritten this functionality a few times for different use cases
+//! and put it into a standalone crate since it might be useful to others.
+//! 
+//! This is used to generate unambiguous one-off mismatch libraries for
+//! a set of DNA sequences.
+//!
+//! ## Usage
+//!
+//! ### Creating a new unambiguous set
+//! 
+//! ```rust
+//! use disambiseq::Disambiseq;
+//! 
+//! let sequences = vec![
+//!     "ACT".to_string(),
+//!     "AGT".to_string()
+//! ];
+//! let dsq = Disambiseq::from_slice(&sequences);
+//! println!("{:#?}", dsq);
+//! ```
+//! 
+//! ### Visualizing the set
+//! 
+//! ```text
+//! Disambiseq {
+//!     unambiguous: {
+//!         "TCT": "ACT",
+//!         "ACA": "ACT",
+//!         "CCT": "ACT",
+//!         "ACC": "ACT",
+//!         "CGT": "AGT",
+//!         "GGT": "AGT",
+//!         "AGA": "AGT",
+//!         "GCT": "ACT",
+//!         "ACG": "ACT",
+//!         "TGT": "AGT",
+//!         "AGC": "AGT",
+//!         "AGT": "ACT",
+//!         "AGG": "AGT",
+//!     },
+//!     parents: {
+//!         "AGT",
+//!         "ACT",
+//!     },
+//!     ambiguous: {
+//!         "ATT",
+//!         "AAT",
+//!     },
+//! }
+//! ```
+//! 
+//! ### Querying the Set
+//! 
+//! ```rust
+//! use disambiseq::Disambiseq;
+//! 
+//! let sequences = vec![
+//!     "ACT".to_string(),
+//!     "AGT".to_string()
+//! ];
+//! let dsq = Disambiseq::from_slice(&sequences);
+//! 
+//! // retrieve a parental sequence
+//! assert_eq!(dsq.get_parent("ACT"), Some(&"ACT".to_string()));
+//! 
+//! // retrieve a mutation sequence's parent
+//! assert_eq!(dsq.get_parent("TCT"), Some(&"ACT".to_string()));
+//! 
+//! // exclude sequences with ambiguous parents
+//! assert_eq!(dsq.get_parent("AAT"), None);
+//! assert_eq!(dsq.get_parent("ATT"), None);
+//! ```
+
 mod disambiseq;
 mod sequence;
 pub use crate::{
