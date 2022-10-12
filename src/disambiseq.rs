0026f49f8c7e41df81abe8218bfedbaa4c4c725d
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-10-11 17:31:37 -0700

    update init and fix parent bug

--- src/disambiseq.rs
+++ src/disambiseq.rs
@@ -8,37 +8,48 @@ pub struct Disambiseq {
     ambiguous: HashSet<String>,
 }
 impl Disambiseq {
-    pub fn from_slice(sequences: &[String]) -> Self {
-        let mut unambiguous = HashMap::new();
-        let mut parents = HashSet::new();
-        let mut ambiguous = HashSet::new();
+    pub fn new() -> Self {
+        Self {
+            unambiguous: HashMap::new(),
+            parents: HashSet::new(),
+            ambiguous: HashSet::new(),
+        }
+    }
 
-        for parent in sequences {
-            parents.insert(parent.to_string());
+    pub fn insert(&mut self, parent: &str) {
+        self.parents.insert(parent.to_string());
 
-            // iterate through all sequence mutations
-            for mutation in Sequence::new(parent).mutate_all() {
-                
-                // skip ambigiuous or parental sequences
-                if ambiguous.contains(&mutation) | parents.contains(&mutation) {
-                    continue
-                }
-                
-                // if the sequence has seen before it becomes ambiguous
-                if unambiguous.contains_key(&mutation) {
-                    ambiguous.insert(mutation.to_string());
-                    unambiguous.remove(&mutation);
-                
-                // purely unambiguous sequence found
-                } else {
-                    unambiguous.insert(mutation.to_string(), parent.to_string());
-                }
+        if self.unambiguous.contains_key(parent) {
+            self.unambiguous.remove(parent);
+        }
+
+        // iterate through all sequence mutations
+        for mutation in Sequence::new(parent).mutate_all() {
+            
+            // skip ambigiuous or parental sequences
+            if self.ambiguous.contains(&mutation) | self.parents.contains(&mutation) {
+                continue
+            }
+            
+            // if the sequence has seen before it becomes ambiguous
+            if self.unambiguous.contains_key(&mutation) {
+                self.ambiguous.insert(mutation.to_string());
+                self.unambiguous.remove(&mutation);
+            
+            // purely unambiguous sequence found
+            } else {
+                self.unambiguous.insert(mutation.to_string(), parent.to_string());
             }
         }
 
-        Self {
-            unambiguous, parents, ambiguous
-        }
+    }
+
+    pub fn from_slice(sequences: &[String]) -> Self {
+        let mut dsq = Self::new();
+        sequences
+            .iter()
+            .for_each(|x| dsq.insert(x));
+        dsq
     }
     pub fn get_parent(&self, seq: &str) -> Option<&String> {
         if let Some(p) = self.parents.get(seq) {
@@ -63,7 +74,7 @@ mod testing {
     use super::Disambiseq;
 
     #[test]
-    fn init() {
+    fn init_slice() {
         let sequences = vec![
             "ACT".to_string(),
             "AGT".to_string()
@@ -71,7 +82,7 @@ mod testing {
         let das = Disambiseq::from_slice(&sequences);
         assert_eq!(das.parents().len(), 2);
         assert_eq!(das.ambiguous().len(), 2);
-        assert_eq!(das.mutations().len(), 13);
+        assert_eq!(das.mutations().len(), 12);
     }
 
     #[test]
@@ -102,5 +113,16 @@ mod testing {
         ];
         let das = Disambiseq::from_slice(&sequences);
         assert_eq!(das.get_parent("ATT"), None);
+    }
+
+    #[test]
+    fn init() {
+        let sequences = vec!["ACT", "AGT"];
+        let mut dsq = Disambiseq::new();
+        dsq.insert(sequences[0]);
+        dsq.insert(sequences[1]);
+        assert_eq!(dsq.parents().len(), 2);
+        assert_eq!(dsq.ambiguous().len(), 2);
+        assert_eq!(dsq.mutations().len(), 12);
     }
 }
